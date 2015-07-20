f1bce25bbe17dbaf1989327e9c09502e828fa59b
Nathan L. Walls <nathan@thoughtbot.com>
2015-07-20 14:50:36 -0400

    WIP: Build an AttributeMapper with FieldMappings

--- app/models/net_suite/attribute_mapper_builder.rb
+++ app/models/net_suite/attribute_mapper_builder.rb
@@ -2,6 +2,8 @@ module NetSuite
   class AttributeMapperBuilder
     attr_accessor :attribute_mapper
 
+    delegate :field_mappings, to: :attribute_mapper
+
     def initialize(user:)
       @attribute_mapper = AttributeMapper.new(
         mapping_direction: "export",
@@ -11,6 +13,28 @@ module NetSuite
 
     def build
       attribute_mapper.save
+      build_field_mappings
+    end
+
+    def default_field_mapping
+      {
+        "email" =>  "email",
+        "first_name" =>  "firstName",
+        "gender" =>  "gender",
+        "last_name" =>  "lastName",
+        "home_phone" =>  "phone",
+      }
+    end
+
+    private
+
+    def build_field_mappings
+      default_field_mapping.each_pair do |namely_field, integration_field|
+        attribute_mapper.field_mappings << FieldMapping.new(
+          integration_field_name: integration_field.to_s,
+          namely_field_name: namely_field
+        )
+      end
     end
   end
 end
--- spec/models/net_suite/attribute_mapper_builder_spec.rb
+++ spec/models/net_suite/attribute_mapper_builder_spec.rb
@@ -2,20 +2,24 @@ require "rails_helper"
 
 def mapped_fields
   {
-    first_name: "firstName",
-    last_name: "lastName",
-    email: "email",
-    gender: "gender",
-    phone: "home_phone",
+    "email" =>  "email",
+    "first_name" => "firstName",
+    "gender" =>  "gender",
+    "home_phone" =>  "phone",
+    "last_name" =>  "lastName",
     # TODO: Subsidiary
     # TODO: title
   }
 end
 
+def new_attribute_mapper_builder
+  NetSuite::AttributeMapperBuilder.new(user: create(:user))
+end
+
 describe NetSuite::AttributeMapperBuilder do
   describe "AttributeMapper" do
     it "returns an AttributeMapper" do
-      builder = NetSuite::AttributeMapperBuilder.new(user: create(:user))
+      builder = new_attribute_mapper_builder
 
       builder.build
 
@@ -23,7 +27,7 @@ describe NetSuite::AttributeMapperBuilder do
     end
 
     it "persists the AttributeMapper" do
-      builder = NetSuite::AttributeMapperBuilder.new(user: create(:user))
+      builder = new_attribute_mapper_builder
 
       builder.build
 
@@ -32,9 +36,28 @@ describe NetSuite::AttributeMapperBuilder do
   end
 
   describe "#field_mappings" do
-    mapped_fields.keys.each do |field|
-      it "has a FieldMapping for #{field}"
+    it "has a FieldMapping for each default field" do
+      builder = new_attribute_mapper_builder
+      builder.build
+
+      integration_mappings = builder.field_mappings.map(
+        &:integration_field_name
+      )
+      namely_mappings = builder.field_mappings.map(
+        &:namely_field_name
+      )
+
+      expect(integration_mappings).to match_array(mapped_fields.values)
+      expect(namely_mappings).to match_array(mapped_fields.keys)
+    end
+
+    it "persists the FieldMappings" do
+      builder = new_attribute_mapper_builder
+      builder.build
+
+      expect(
+        builder.attribute_mapper.field_mappings.reject(&:persisted?)
+      ).to be_empty
     end
   end
 end
-
