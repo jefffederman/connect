e0ec9d69f52769a688a43001fdddbbc9978b5436
Rafael George <george.rafael@gmail.com>
2016-01-11 11:09:32 -0500

    Add logic for when the reports to is empty
    If some profiles does not report to any other profile then the order
    does not matter.

--- app/services/net_suite/profiles_sorter.rb
+++ app/services/net_suite/profiles_sorter.rb
@@ -1,17 +1,41 @@
 module NetSuite
   class ProfilesSorter
-
     def initialize(profiles:)
       @profiles = profiles
     end
 
     def call
       profiles.sort_by do |profile|
-        profile.reports_to.first.fetch(:id) <=> profile.guid
+        ReportTo.for(profile) <=> profile.guid
       end
     end
 
     private
+
+    class ReportTo
+      include Comparable
+
+      def self.for(profile)
+        unless profile.reports_to.empty?
+          new(guid: profile.reports_to.first.fetch(:id, -1))
+        else
+          new(guid: -1)
+        end
+      end
+
+      attr_reader :guid
+      def initialize(guid:)
+        @guid = guid
+      end
+
+      def <=>(other_guid)
+        guid <=> other_guid
+      end
+
+      private
+
+      attr_reader :guid
+    end
 
     attr_reader :profiles
   end
--- spec/services/net_suite/profiles_sorter_spec.rb
+++ spec/services/net_suite/profiles_sorter_spec.rb
@@ -2,12 +2,12 @@ require_relative "../../../app/services/net_suite/profiles_sorter"
 
 describe NetSuite::ProfilesSorter do
   subject(:sorter) { described_class.new(profiles: profiles) }
-  let(:profiles) do
-    [profile_with_manager, profile_manager, another_profile_with_manager]
-  end
 
   describe "#to_a" do
     context "when profiles have reports to another profile" do
+      let(:profiles) do
+        [profile_with_manager, profile_manager, another_profile_with_manager]
+      end
       let(:profile_with_manager) do
         double :profile_with_manager,
           guid: "profile_guid",
@@ -23,11 +23,41 @@ describe NetSuite::ProfilesSorter do
           guid: "my_guid",
           reports_to: [ id: "another_guid_manager" ]
       end
+
       it "returns an array with the manager before the one who reports to" do
         expect(sorter.call).to match_array [
           profile_manager,
           profile_with_manager,
           another_profile_with_manager
+        ]
+      end
+    end
+
+    context "when profiles doesn't have reports to" do
+      let(:profiles) do
+        [profile_without_manager, profile_manager, another_profile_without_manager]
+      end
+      let(:profile_without_manager) do
+        double :profile_without_manager,
+          guid: "profile_guid",
+          reports_to: [ ]
+      end
+      let(:another_profile_without_manager) do
+        double :another_profile_without_manager,
+          guid: "another_profile_guid",
+          reports_to: [ ]
+      end
+      let(:profile_manager) do
+        double :manager,
+          guid: "my_guid",
+          reports_to: [ ]
+      end
+
+      it "returns an array with the profiles" do
+        expect(sorter.call).to match_array [
+          profile_without_manager,
+          profile_manager,
+          another_profile_without_manager
         ]
       end
     end
